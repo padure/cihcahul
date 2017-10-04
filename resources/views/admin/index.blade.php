0197078e9cb017c1f39b45fb363a7c342adc88e4
lygalas <lygalas@lygalas-anonim>
2017-10-04 21:26:15 +0200

    Design nou Admin Panel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.app')
 @section('content')
-<h2>Admin Panell</h2>
+<div class="container admin-body col-md-8 col-md-offset-2">
+    <div class="col-md-12 admin">
+        <div class="col-md-3 color-1">
+            <img src="{{URL::asset('img/sistems/logo/if_home_126572.png')}}" alt="Imaginea lipseste">
+            <h4 class="text-center">Acasa</h4>
+        </div>
+        <div class="col-md-3 color-2"><img src="{{URL::asset('img/sistems/logo/if_student_309036.png')}}" alt="Imaginea lipseste">
+        <h4 class="text-center">Specialitati</h4></div>
+        <div class="col-md-3 color-1"><img src="{{URL::asset('img/sistems/logo/if_add_126583.png')}}" alt="Imaginea lipseste">
+        <h4 class="text-center">Admitere</h4></div>
+        <div class="col-md-3 color-2"><img src="{{URL::asset('img/sistems/logo/if_39_Calendar_106206.png')}}" alt="Imaginea lipseste">
+        <h4 class="text-center">Evenimente</h4></div>
+    </div>
+    <div class="col-md-12 admin">
+        <div class="col-md-3 color-2"><img src="{{URL::asset('img/sistems/logo/if_info_293683.png')}}" alt="Imaginea lipseste">
+        <h4 class="text-center">Despre</h4></div>
+        <div class="col-md-3 color-1"><img src="{{URL::asset('img/sistems/logo/if_tags_1608942.png')}}" alt="Imaginea lipseste">
+        <h4 class="text-center">Avize</h4></div>
+        <div class="col-md-3 color-2"><img src="{{URL::asset('img/sistems/logo/if_084_Photo_183212.png')}}" alt="Imaginea lipseste">
+        <h4 class="text-center">Galerie</h4></div>
+        <div class="col-md-3 color-1"><img src="{{URL::asset('img/sistems/logo/if_earphone_461372.png')}}" alt="Imaginea lipseste">
+        <h4 class="text-center">Contacte</h4></div>
+    </div>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/admin-style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -43,22 +44,86 @@
                     <ul class="nav navbar-nav">
                         <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                  Evenimente<span class="caret"></span>
+                                    <span class="glyphicon glyphicon-home" aria-hidden="true"></span> Acasa<span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
-                                    <li><a href="#">Evenimente</a></li>
+                                    <li><a href="#">Sidebar</a></li>
+                                    <li><a href="#">Parteneri</a></li>
+                                    <li><a href="#">Info footer</a></li>
+                                </ul>
+                        </li>
+                        <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                  <span class="glyphicon glyphicon-education" aria-hidden="true"></span> Specialitati<span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">List Specialitati</a></li>
+                                    <li><a href="#">Adaugare Specialitati</a></li>
+                                </ul>
+                        </li>
+                         <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Admitere<span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Regulament</a></li>
+                                    <li><a href="#">Rezultate</a></li>  
+                                </ul>
+                        </li>
+                        <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Evenimente<span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">List Evenimente</a></li>
                                     <li><a href="#">Creare Evenimente</a></li>
                                 </ul>
                         </li>
                         <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                  Profesori<span class="caret"></span>
+                                  <span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Despre<span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
-                                    <li><a href="#">Evenimente</a></li>
-                                    <li><a href="#">Creare Evenimente</a></li>
+                                    <li><a href="#">Nr. Elevi</a></li>
+                                    <li><a href="#">Administratie</a></li>
+                                    <li><a href="#">Add Administratie</a></li>
+                                </ul>
+                        </li>
+                        <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                  <span class="glyphicon glyphicon-tags" aria-hidden="true"></span> Avize<span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">List Avize</a></li>
+                                    <li><a href="#">Creare Aviz</a></li>
+                                </ul>
+                        </li>
+                        <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                  <span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Galerie<span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">List Galerie</a></li>
+                                    <li><a href="#">Add imagine</a></li>
+                                </ul>
+                        </li>
+                        <li><a href="#"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span> Add Meniu</a></li>
+                         <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                  <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> Contacte<span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Detalii</a></li>
+                                    <li><a href="#">Ore de lucru</a></li>
+                                    <li><a href="#">Mesaje</a></li>
                                 </ul>
                         </li>
                     </ul>
